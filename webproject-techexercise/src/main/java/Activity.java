377084c2a9a19d3ac2edf70ab212dd9ae1a5e055
kfuta <kfuta@tutanota.com>
2024-03-14 17:59:44 -0600

    Created external style sheet. Created "AddToLibrary.html"

--- webproject-techexercise/src/main/java/Activity.java
+++ webproject-techexercise/src/main/java/Activity.java
@@ -31,40 +31,13 @@ public class Activity extends HttpServlet {
       String tableName1 = "Your Activity";
       String docType = "<!doctype html public \"-//w3c//dtd html 4.0 transitional//en\">\n"; 
       out.println(docType + "<html>\n" + "<head><title>" + title + "</title>\n"); // start of head; start and end of title
-      out.println("<style>\n" // start of style
-      		+ "body {\n background-color: #F1FAEE;\n}\n"
-            + ".center {\n"	// .center style
-            	+ "margin-left: auto;\n"
-            	+ "margin-right: auto;\n}\n"
-            + "table {\n"	// table style
-            	+ "border-collapse: collapse;\n"
-            	+ "width: 80%;\n}\n"
-            + "th, td {border-bottom: 1px solid;\n"		// table header and data style
-            	+ "border-color: #1D3557;\n"
-            	+ "padding: 5px;\n"
-            	+ "text-align: left;\n}\n"
-            + "th {background-color: #A8DADC;\n}\n"		// table header style
-            + "tr:hover {background-color: #E63946;\n}\n"	// table row hover feature
-            + "header {background-color: #1D3557;\n"	// header style
-            	+ "color: #E63946;\n"
-            	+ "font-family: Trebuchet MS;\n"
-            	+ "text-align:center;"
-            	+ "padding:5px;\n}\n"
-            + "nav {\n"		// navigation style
-            	+ "line-height:30px;\n"
-            	+ "background-color: #457B9D;\n"
-            	+ "width:100px;\n"
-            	+ "height:700px;\n"
-            	+ "float:left;\n"
-            	+ "padding:5px;\n}\n"
-            + "a {color: #F1FAEE\n"		// link style
-            + "</style>\n"	// end of style
-            + "</head>\n" 	// end of head
+      out.println("<link rel=\"stylesheet\" href=\"style.css\">");	// stylesheet
+      out.println("</head>\n" 	// end of head
             + "<body>\n" 	// start of body
-            + "<header><h1 align=\"center\">" + title + "</h1></header>\n"	// first header
-            + "<nav> <a href=/webproject-techexercise/Library>Library</a> <br>"	// navigation links; remember to change to 4830- when you deploy
-            + "<a href=/webproject-techexercise/Shelves>Shelves</a> <br> "
-            + "<a href=/webproject-techexercise/Activity>Activity</a> <br> </nav>"
+            + "<header><h1 align=\"center\">&#128218; " + title + " &#128218;</h1></header>\n"	// first header
+            + "<nav> <a href=/webproject-techexercise/Library class=mainNav>Library</a> <br>"	// navigation links; remember to change to 4830- when you deploy
+            + "<a href=/webproject-techexercise/Shelves class=mainNav>Shelves</a> <br> "
+            + "<a href=/webproject-techexercise/Activity class=mainNav>Activity</a> <br></nav>"
             + "<h2 align=\"center\">" + tableName1 + "</h2>\n");	// first table name
       out.println("<table class=\"center\"> <tr> <th style=\"width:20%\">Book Title</th> "	// first table
       		+ "<th style=\"width:20%\">Book Author</th> <th style=\"width:20%\">Date Started</th> "
