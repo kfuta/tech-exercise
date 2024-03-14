c0b112e544b3762fa98617d044d682b3cf0ced8e
kfuta <kfuta@tutanota.com>
2024-03-14 13:36:03 -0600

    Improved code readability. Changed Home.java to Library.java. Separated library and shelves tables into Library.java and Shelves.java. Added extra columns to books database table (see Library.java)

---
+++ webproject-techexercise/src/main/java/Library.java
@@ -0,0 +1,127 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Library")
+public class Library extends HttpServlet {
+   private static final long serialVersionUID = 1L;
+
+   public Library () {
+      super();
+   }
+
+   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      tableContents(response);
+   }
+
+   void tableContents(HttpServletResponse response) throws IOException {
+      // HTML and CSS formatting
+	  response.setContentType("text/html");
+      PrintWriter out = response.getWriter();
+      String title = "Reading Tracker";
+      String tableName1 = "Your Library";
+      String docType = "<!doctype html public \"-//w3c//dtd html 4.0 transitional//en\">\n"; 
+      out.println(docType + "<html>\n" + "<head><title>" + title + "</title>\n"); // start of head; start and end of title
+      out.println("<style>\n" // start of style
+      		+ "body {\n background-color: #F1FAEE;\n}\n"
+            + ".center {\n"	// .center style
+            	+ "margin-left: auto;\n"
+            	+ "margin-right: auto;\n}\n"
+            + "table {\n"	// table style
+            	+ "border-collapse: collapse;\n"
+            	+ "width: 80%;\n}\n"
+            + "th, td {border-bottom: 1px solid;\n"		// table header and data style
+            	+ "border-color: #1D3557;\n"
+            	+ "padding: 5px;\n"
+            	+ "text-align: left;\n}\n"
+            + "th {background-color: #A8DADC;\n}\n"		// table header style
+            + "tr:hover {background-color: #E63946;\n}\n"	// table row hover feature
+            + "header {background-color: #1D3557;\n"	// header style
+            	+ "color: #E63946;\n"
+            	+ "font-family: Trebuchet MS;\n"
+            	+ "text-align:center;"
+            	+ "padding:5px;\n}\n"
+            + "nav {\n"		// navigation style
+            	+ "line-height:30px;\n"
+            	+ "background-color: #457B9D;\n"
+            	+ "width:100px;\n"
+            	+ "height:700px;\n"
+            	+ "float:left;\n"
+            	+ "padding:5px;\n}\n"
+            + "a {color: #F1FAEE\n"		// link style
+            + "</style>\n"	// end of style
+            + "</head>\n" 	// end of head
+            + "<body>\n" 	// start of body
+            + "<header><h1 align=\"center\">" + title + "</h1></header>\n"	// first header
+            + "<nav> <a href=/webproject-techexercise/Library>Library</a> <br>"	// navigation links; remember to change to 4830- when you deploy
+            + "<a href=/webproject-techexercise/Shelves>Shelves</a> <br> </nav>"
+            + "<h2 align=\"center\">" + tableName1 + "</h2>\n");	// first table name
+      out.println("<table class=\"center\"> <tr> <th style=\"width:20%\">Book Title</th> "	// first table
+      		+ "<th style=\"width:20%\">Book Author</th> <th style=\"width:20%\">Genre</th> "
+      		+ "<th style=\"width:20%\">Year First Published</th> <th style=\"width:20%\">Book Page Count</th></tr>");
+
+      
+      Connection connection = null;
+      PreparedStatement preparedStatement = null;
+      try {
+         // open database connection
+    	 DBConnectionFutakami.getDBConnection();
+         connection = DBConnectionFutakami.connection;	
+
+         // print books table
+         String selectSQL = "SELECT * FROM books ORDER By title";
+         preparedStatement = connection.prepareStatement(selectSQL);
+         ResultSet rs = preparedStatement.executeQuery();
+
+         while (rs.next()) {
+            String bookTitle = rs.getString("title").trim();
+            String bookAuthor = rs.getString("author").trim();
+            String bookGenre = rs.getString("genre").trim();
+            int bookYearFirstPublished = rs.getInt("yearFirstPublished");
+            int bookPageCt = rs.getInt("pageCt");
+
+            out.println("<tr><td>" + bookTitle + "</td>");
+            out.println("<td>" + bookAuthor + "</td>");
+            out.println("<td>" + bookGenre + "</td>");
+            out.println("<td>" + bookYearFirstPublished + "</td>");
+            out.println("<td>" + bookPageCt + "</td></tr>");
+         }
+         out.println("</table>");
+         rs.close();
+         preparedStatement.close();
+         
+         out.println("</body></html>");
+         connection.close();
+      } catch (SQLException se) {
+         se.printStackTrace();
+      } catch (Exception e) {
+         e.printStackTrace();
+      } finally {
+         try {
+            if (preparedStatement != null)
+               preparedStatement.close();
+         } catch (SQLException se2) {
+         }
+         try {
+            if (connection != null)
+               connection.close();
+         } catch (SQLException se) {
+            se.printStackTrace();
+         }
+      }
+   }
+
+   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      doGet(request, response);
+   }
+
+}
--- webproject-techexercise/src/main/java/Shelves.java
+++ webproject-techexercise/src/main/java/Shelves.java
@@ -11,11 +11,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/Home")
-public class Home extends HttpServlet {
+@WebServlet("/Shelves")
+public class Shelves extends HttpServlet {
    private static final long serialVersionUID = 1L;
 
-   public Home () {
+   public Shelves () {
       super();
    }
 
@@ -28,77 +28,46 @@ public class Home extends HttpServlet {
 	  response.setContentType("text/html");
       PrintWriter out = response.getWriter();
       String title = "Reading Tracker";
-      String tableName1 = "Your Library";
       String tableName2 = "Books You Are Currently Reading";
       String tableName3 = "Books You Plan to Read";
       String tableName4 = "Books You've Read";
       String docType = "<!doctype html public \"-//w3c//dtd html 4.0 transitional//en\">\n"; 
-      out.println(docType 
-            + "<html>\n" 
-            + "<head><title>" + title + "</title>\n" // start and end of title
-            + "<style> body {\n"	// start of style
-            + "background-color: #F1FAEE;\n}\n"
-            + ".center {\n"
-            + "margin-left: auto;\n"
-            + "margin-right: auto;\n}\n"
-            + "table {\n"
-            + "border-collapse: collapse;\n"
-            + "width: 80%;\n}\n"
-            + "th, td {border-bottom: 1px solid;\n"
-            + "border-color: #1D3557;\n"
-            + "padding: 5px;\n"
-            + "text-align: left;\n}\n"
-            + "th {background-color: #A8DADC;\n}\n"
-            + "tr:hover {background-color: #E63946;\n}\n"
-            + "header {background-color: #1D3557;\n"
-            + "color: #F1FAEE;\n"
-            + "text-align:center;"
-            + "padding:5px;\n}\n"
-            + "section {\n"
-            + "width: 350px;\n"
-            + "float: left;\n"
-            + "padding 10px;\n}\n"
-            + "nav {\n"
-            + "line-height:30px;\n"
-            + "background-color: #457B9D;\n"
-            + "width:100px;\n"
-            + "height:700px;\n"
-            + "float:left;\n"
-            + "padding:5px;\n}\n"
-            + "a {color: #F1FAEE\n"
+      out.println(docType + "<html>\n" + "<head><title>" + title + "</title>\n"); // start of head; start and end of title
+      out.println("<style>\n" // start of style
+      		+ "body {\n background-color: #F1FAEE;\n}\n"
+            + ".center {\n"	// .center style
+            	+ "margin-left: auto;\n"
+            	+ "margin-right: auto;\n}\n"
+            + "table {\n"	// table style
+            	+ "border-collapse: collapse;\n"
+            	+ "width: 80%;\n}\n"
+            + "th, td {border-bottom: 1px solid;\n"		// table header and data style
+            	+ "border-color: #1D3557;\n"
+            	+ "padding: 5px;\n"
+            	+ "text-align: left;\n}\n"
+            + "th {background-color: #A8DADC;\n}\n"		// table header style
+            + "tr:hover {background-color: #E63946;\n}\n"	// table row hover feature
+            + "header {background-color: #1D3557;\n"	// header style
+            	+ "color: #E63946;\n"
+            	+ "font-family: Trebuchet MS;\n"
+            	+ "text-align:center;"
+            	+ "padding:5px;\n}\n"
+            + "nav {\n"		// navigation style
+            	+ "line-height:30px;\n"
+            	+ "background-color: #457B9D;\n"
+            	+ "width:100px;\n"
+            	+ "height:700px;\n"
+            	+ "float:left;\n"
+            	+ "padding:5px;\n}\n"
+            + "a {color: #F1FAEE\n"		// link style
             + "</style>\n"	// end of style
-            + "</head>\n" 
-            + "<body>\n" 
-            + "<header><h1 align=\"center\">" + title + "</h1></header>\n"	// first headers and table
-            + "<nav> <a href=/4830-webproject-techexercise/Home>Home</a> <br>"
-            + "<a href=/4830-webproject-techexercise/library_search.html>Search Data</a> <br> </nav>"
-            + "<h2 align=\"center\">" + tableName1 + "</h2>\n");
-      out.println("<table class=\"center\"> <tr> <th style=\"width:33.3%\">Book Title</th> "
-      		+ "<th style=\"width:33.3%\">Book Author</th> <th style=\"width:33.3%\">Book Page Count</th> </tr>");
-    
-      /*
-nav {
-    line-height:30px;
-    background-color:#eeeeee;
-    height:300px;
-    width:100px;
-    float:left;
-    padding:5px;	      
-}
-section {
-    width:350px;
-    float:left;
-    padding:10px;	 	 
-}
-footer {
-    background-color:black;
-    color:white;
-    clear:both;
-    text-align:center;
-    padding:5px;	 	 
-}
-       */
+            + "</head>\n" 	// end of head
+            + "<body>\n" 	// start of body
+            + "<header><h1 align=\"center\">" + title + "</h1></header>\n"	// first header
+            + "<nav> <a href=/webproject-techexercise/Library>Library</a> <br>"	// navigation links; remember to change to 4830- when you deploy
+            + "<a href=/webproject-techexercise/Shelves>Shelves</a> <br> </nav>");
 
+     
       Connection connection = null;
       PreparedStatement preparedStatement = null;
       try {
@@ -106,31 +75,13 @@ footer {
     	 DBConnectionFutakami.getDBConnection();
          connection = DBConnectionFutakami.connection;	
 
-         // print books table
-         String selectSQL = "SELECT * FROM books ORDER By title";
-         preparedStatement = connection.prepareStatement(selectSQL);
-         ResultSet rs = preparedStatement.executeQuery();
-
-         while (rs.next()) {
-            String bookTitle = rs.getString("title").trim();
-            String bookAuthor = rs.getString("author").trim();
-            int bookPageCt = rs.getInt("pageCt");
-
-            out.println("<tr><td>" + bookTitle + "</td>");
-            out.println("<td>" + bookAuthor + "</td>");
-            out.println("<td>" + bookPageCt + "</td></tr>");
-         }
-         out.println("</table>");
-         rs.close();
-         preparedStatement.close();
-         
-         //print bookInstance table where shelfType = "Currently Reading"
+         // print bookInstance table where shelfType = "Currently Reading"
          out.println("<h2 align=\"center\">" + tableName2 + "</h2>\n");
          out.println("<table class=\"center\"> <tr> <th style=\"width:20%\">Book Title</th> <th style=\"width:20%\">Book Author</th>"
          + "<th style=\"width:20%\">Date Started</th> <th style=\"width:20%\">Date Completed</th> <th style=\"width:20%\">Rating</th></tr>");
-         selectSQL = "SELECT * FROM bookInstance WHERE shelfType = 'Currently Reading'";
+         String selectSQL = "SELECT * FROM bookInstance WHERE shelfType = 'Currently Reading'";
          preparedStatement = connection.prepareStatement(selectSQL);
-         rs = preparedStatement.executeQuery();
+         ResultSet rs = preparedStatement.executeQuery();
          
          while (rs.next()) {
              String bookTitleFk = rs.getString("titleFk").trim();
